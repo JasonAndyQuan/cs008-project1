f43d6dab1edf6a2216c1872f8c02c14fa2cbb4aa
Jason <jasonquan30@gmail.com>
2024-09-24 10:42:22 -0700

    main and inputs set up

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char input = 'y';
+    string course, enrollment;
+
+    cout << "Enter course filename : ";
+    cin >> course;
+    cout << "Enter enrollment filename : ";
+    cin >> enrollment;
+
+    do {
+        cout << "\n============== Menu ==============\n";
+        cout << "1. View your registration\n";
+        cout << "2. Course registration\n";
+        cout << "3. Course cancellation\n";
+        cout << "4. Print enrollment list inclusing waitlist\n";
+        cout << "5. Exit\n";
+        cout << "---> Select : ";
+
+        cin >> input;
+        switch (input){
+            case ('1'):
+                break;
+            case ('2'):
+                ///asd
+                break;
+            case ('3'):
+                ///asd
+                break;
+            case ('4'):
+                ///asd
+                break;
+            case ('5'):
+                ///asd
+                break;
+            default:
+                cout << "\n invalid input \n";
+                break;
+            }
+    } while (input != 5);
+}
